ef3c7fcea5ba2837db2d2f70efaf8424526b8ce7
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-03-17 14:16:10 +0800

    UPDATE

--- mockquizapp/mockquizapp/settings.py
+++ mockquizapp/mockquizapp/settings.py
@@ -125,6 +125,14 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# Email settings
+
+
+# Media files (uploaded files)
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
