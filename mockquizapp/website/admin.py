6691e74f98adb4d1f1a6b4afb6c3853c8972efab
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-03-17 21:01:03 +0800

    update model

--- mockquizapp/website/admin.py
+++ mockquizapp/website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(AdminData)
+admin.site.register(StudentData)
+admin.site.register(QuizData)
+
--- mockquizapp/website/models.py
+++ mockquizapp/website/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+class StudentData(models.Model):
+    fname = models.CharField(max_length=50 , blank=True, default=None, null=True)
+    mname = models.CharField(max_length=50 , blank=True, default=None, null=True)
+    lname = models.CharField(max_length=50 , blank=True, default=None, null=True)
+    school = models.CharField(max_length=50 , blank=True, default=None, null=True)
+    address = models.CharField(max_length=50 , blank=True, default=None, null=True)
+    gmail = models.CharField(max_length=50 , blank=True, default=None, null=True)
+    phone = models.CharField(max_length=50 , blank=True, default=None, null=True)
+    admin_id = models.IntegerField(blank=True, default=None, null=True)
+
+    def __str__(self):
+        return f"Student: {self.fname} {self.mname} {self.lname}"
+    
+    def get_data(self) -> dict:
+        return {
+            'fname': self.fname,
+            'lname': self.lname,
+            'address': self.address,
+            'gmail': self.gmail,
+            'phone': self.phone
+        }
+
+
+class AdminData(models.Model):
+    username = models.CharField(max_length=50 , blank=True, default=None, null=True)
+    password = models.CharField(max_length=50  , blank=True, default=None, null=True)
+    
+    def __str__(self):
+        return f"Admin: {self.username}"
+
+
+class QuizData(models.Model):
+    number_of_correct = models.IntegerField( blank=True, default=None, null=True)
+    number_of_wrong = models.IntegerField( blank=True, default=None, null=True)
+    student_id = models.IntegerField( blank=True, default=None, null=True)
+    file = models.FileField( blank=True, default=None, null=True)
+    
+    def __str__(self):
+        return f"Score: {self.score} - {self.student_id}"
+    
+    def get_data(self) -> dict:
+        return {
+            'number_of_correct': self.number_of_correct,
+            'number_of_wrong': self.number_of_wrong,
+            'student_id': self.student_id,
+            'question_id': self.question_id,
+            'file': self.file.path if self.file else None
+        }
+    
+    
