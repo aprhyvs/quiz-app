73b8b71d31c6cf549e474d8729e3b2db74cee3f3
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-03-18 10:52:43 +0800

    add api routes for admin

--- mockquizapp/website/urls.py
+++ mockquizapp/website/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('api/login/admin', view=login_admin),
     path('api/logout', view=logout),
     path('api/register/student', view=register_student),
+    
+    path('api/admin/students', view=get_list_of_students),
+    path('api/admin/delete/student', view=delete_student),
+    path('api/admin/update/student', view=update_student_data),
 
     #=============================== File Upload ===============================#
     path("upload/", upload_file, name="upload_file"),
