6ec6dd881bb57c8436f6fd7bca9a54b41cf6846b
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-03-27 23:20:16 +0800

    fix visit

---
+++ mockquizapp/static/javascript/admin_visit/scripts.js
@@ -0,0 +1,33 @@
+
+
+document.addEventListener('DOMContentLoaded', async function () {
+
+    const studentID = sessionStorage.getItem('studentID');
+    
+    if (studentID) {
+        res = await getDataFromUrlWithParams(`/api/admin/get/studentdata`,{
+            'student_id': studentID
+        });
+
+        document.getElementById("title-nav").textContent = res?.username ?? "N/A";
+    }
+
+
+    document.getElementById("open-logout-form").addEventListener("click", async function (event) { 
+        event.preventDefault(); 
+        document.getElementById("logout-form-pop").style.display = "flex"; 
+    });
+    document.getElementById("logout-but").addEventListener("click", async function (event) { 
+        event.preventDefault(); 
+        document.getElementById("logout-form-pop").style.display = "none"; 
+        const res = await getDataFromUrl("/api/logout");
+        if (res){   
+            window.location.href = "../";
+        }
+    });
+    document.getElementById("cancel-logout-but").addEventListener("click", async function (event) { 
+        event.preventDefault();
+        document.getElementById("logout-form-pop").style.display = "none"; 
+    });
+
+});
