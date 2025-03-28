11de84a741a641f071011a50e8429616d2222105
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2025-03-28 21:04:06 +0800

    Add quiz_page
    - not fully functional yet.

--- mockquizapp/website/urls.py
+++ mockquizapp/website/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('student_dashboard/', student_dashboard, name='student-dashboard'),
     path('admin_editor/', admin_editor, name='admin-editor'),
     path('student_analytics/', student_analytics, name='student-analytics'),
+    path('student_quizpage/', student_quizpage, name='student-quizpage'),
 
     #=============================== API Endpoints ===============================#
     path('api/login/student', view=login_student),
