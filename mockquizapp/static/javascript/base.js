dde47da17f633075353687651c4f2f0b08b9eb40
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-04-06 14:00:52 +0800

    put onlod

--- mockquizapp/static/javascript/base.js
+++ mockquizapp/static/javascript/base.js
@@ -78,23 +78,25 @@ async function generateVoiceMessage(textMessage){
 }
 
 // dark theme toggle
+document.addEventListener('DOMContentLoaded', () => {
+    let darkmode = localStorage.getItem('darkmode')
+    const themeSwitch = document.getElementById('theme-switch')
 
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
+    const enableDarkmode = () => {
+        document.body.classList.add('darkmode')
+        localStorage.setItem('darkmode', 'active')
+    }
 
-const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-}
+    const disableDarkmode = () => {
+        document.body.classList.remove('darkmode')
+        localStorage.setItem('darkmode', null)
+    }
 
-const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
-}
+    if (darkmode === "active") enableDarkmode()
 
-if (darkmode === "active") enableDarkmode()
+    themeSwitch.addEventListener("click", () => {
+        darkmode = localStorage.getItem('darkmode')
+        darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    })
+});
 
-themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode')
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
