35dcedddbeb6127eca75a59d65b04e576969a092
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-04-07 22:55:01 +0800

    fix favicon

--- mockquizapp/website/urls.py
+++ mockquizapp/website/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import RedirectView
-
-from django.urls import re_path
-
-favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 from .views import *
 
 
 urlpatterns = [
-    re_path(r'^favicon\.ico$', favicon_view),
 
     path('api/student/upload/stage1', view=upload_file_view_status_1),
     path('api/student/upload/stage2', view=upload_file_view_status_2),
