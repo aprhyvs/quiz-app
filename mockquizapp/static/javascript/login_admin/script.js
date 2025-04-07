8283d0266825d6a19e9d17ea9f42dd7a645308d9
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-04-07 11:01:50 +0800

    fix bugs in login admin

--- mockquizapp/static/javascript/login_admin/script.js
+++ mockquizapp/static/javascript/login_admin/script.js
@@ -1,11 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("loginForm"); 
+document.addEventListener("DOMContentLoaded", function () { 
+    const edit_form_pop = document.getElementById("edit-form-pop");
+    const formtag = document.getElementById("loginForm");
 
-    form.addEventListener("submit", async function (event) {
+    document.getElementById("understand-but").addEventListener("click", async function (event) {
+        event.preventDefault(); 
+        edit_form_pop.style.display = "none"; 
+    });
+
+    formtag.addEventListener("submit", async function (event) {
         event.preventDefault();
 
         // Create FormData object from form fields
-        const formData = new FormData(form);
+        const formData = new FormData(formtag);
 
         try {
             const response = await fetch("/api/login/admin", {  // Adjust endpoint if needed
@@ -20,18 +26,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
             if (response.ok) {
                 // responseMessage.textContent = "Login successful!";
-                // responseMessage.style.color = "var(--green)";
+                // responseMessage.style.color = "var(--green)"; 
                 window.location.href = adminDashboardUrl;
-                form.reset();
+                formtag.reset();
             } else {
                 // responseMessage.textContent = data.error || "Login failed!";
                 // responseMessage.style.color = "var(--red)";
+                document.getElementById("edit-text").textContent = `The username or password is incorrect.`;
+                edit_form_pop.style.display = "flex";
             }
         } catch (error) {
             // responseMessage.textContent = "An error occurred. Please try again.";
             // responseMessage.style.color = "var(--red)";
+            document.getElementById("edit-text").textContent = `An error occurred. Please try again.`;
+            edit_form_pop.style.display = "flex";
+
         }
     });
 
     // Function to get CSRF token from Django
+
 });
