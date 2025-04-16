802d9502ea6d387b2c220b1bb73f5b9d4db57ffa
karrothub <joennarvarte@gmail.com>
2025-04-17 02:24:02 +0800

    Urls

--- mockquizapp/mockquizapp/urls.py
+++ mockquizapp/mockquizapp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('a', include('website.urls')),  # Include home app URLs
+    path('', include('website.urls')),  # Include home app URLs
 ]
